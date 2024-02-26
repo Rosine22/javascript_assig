c0efd91dd7025489a178fdeb4fd89013f3f5212d
Rosine22 <116634586+Rosine22@users.noreply.github.com>
2024-02-26 00:10:20 -0800

    Update Whale.js

---
+++ Whale.js
@@ -0,0 +1,32 @@
+const input = 'electricity';
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  
+  for (let j = 0; j < vowels.length; j++) {
+
+    if (input[i] === vowels[j]) {
+      
+      resultArray.push(input[i]);
+
+    
+      if (input[i] === 'e') {
+        resultArray.push('e');
+      }
+
+     
+      if (input[i] === 'u') {
+        resultArray.push('u');
+      }
+    }
+  }
+}
+
+console.log(resultArray);
+
+
+const resultString = resultArray.join('').toUpperCase();
+
+console.log(resultString);
